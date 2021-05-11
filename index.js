e063d00b41f6b30a515c1bad202909d053545464
Krzysztof Staroń <kisiel3141@gmail.com>
2021-05-11 23:45:13 +0200

    English language

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const { MessageEmbed } = require('discord.js');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const permisionRole="FarmBotManager";
-let debuging=false;
 let plantsData;
 let playerData;
 
@@ -24,22 +23,20 @@ client.on('message', msg => {
   const getMeasage = function() {return msg.content.toLowerCase();}
   const getCommand = function() {return msg.content.split(" ");}
   const send = function(txt) {msg.channel.send(txt);}
-  const noPermision= function(){send("tylko "+permisionRole+" może używać tej komendy");}
-  const wallet = function(){send("masz: "+playerData[msg.author.id].money)}
+  const noPermision= function(){send("only "+permisionRole+" can use this command");}
 
   const aded = function(){
     for (var i = 0; i < Object.keys(plantsData).length; i++) {
       const show = plantsData[Object.keys(plantsData)[i]];
-      send(`Nazwa:**${Object.keys(plantsData)[i]}**, BuyPrice:**${show.buyPrice}**zł, growTime:**${show.growTime}**milisec, SellPrice:**${show.sellPrice}**zł`);
+      send(`Name:**${Object.keys(plantsData)[i]}**, BuyPrice:**${show.buyPrice}**$, growTime:**${show.growTime}**milisec, SellPrice:**${show.sellPrice}**$`);
     }
     if (Object.keys(plantsData).length==0) {
-      send("brak roślin");
+      send("There is no plant in the system");
     }
   }
   /*inne*/
 
   if (!playerData.hasOwnProperty(msg.author.id)) {
-    if (debuging) {send("playerData nie zawiera:"+msg.author+"\n"+msg.author.username);}
     playerData[msg.author.id]={
       "surname":msg.author.username,
       "money":0,
@@ -48,7 +45,6 @@ client.on('message', msg => {
         "plants":[]
       }
     }
-    if (debuging) {send("wygenerowano"+msg.author+"w playerData")};
   }
 
   /*commands*/
@@ -62,28 +58,15 @@ client.on('message', msg => {
 
   }
 
-  if (getCommand()[0]=="#debuging") {
-    if (permision) {
-      if (getCommand().length!=2) {
-        send("{true/false}");
-      }else if(!(getCommand()[1]=="false" || getCommand()[1]=="true")){
-        send("{true/false}");
-      }else{
-        debuging=getCommand()[1]=="true";
-        send('debuging is now set to **'+debuging+'**');
-      }
-    }else{noPermision()}
-  }
-
   if (getCommand()[0]=="#giveMoney") {
     if (permision) {
       if (getCommand().length!=2) {
-        send("{ile?}");
+        send("{how many?}");
       }else if(!parseInt(getCommand()[1])>0){
-        send("liczba!!");
+        send("number!!");
       }else{
         playerData[msg.author.id].money+=parseInt(getCommand()[1]);
-        wallet();
+        send("You have:"+playerData[msg.author.id].money);
       }
     }else{noPermision()}
   }
@@ -91,17 +74,13 @@ client.on('message', msg => {
   if (getCommand()[0]=="#DelatePlant") {
     if (permision) {
      if (getCommand().length!=2){
-      send("{nazwa}");
+      send("{name}");
      }else if(!plantsData.hasOwnProperty(getCommand()[1])){
-      send("nie ma takiej rośliny");
+      send("plant don't exist");
      }else{
-      send("usunięto:"+ getCommand()[1]);
-      send("pozostałe rośliny:");
+      send("delete:"+ getCommand()[1])+"\n other plants:";
       delete plantsData[getCommand()[1]];
       aded();
-      if (Object.keys(plantsData).length==0) {
-        send("brak roślin");
-      }
     }
   }else{noPermision()}
   }
@@ -109,15 +88,15 @@ client.on('message', msg => {
   if (getCommand()[0]=="#AdPlant") {
     if (permision) {
     if (getCommand().length!=5){
-      send("{nazwa} {cenaKupnaNasion} {czas rośnięcia} {cenaSprzedarzy}");
+      send("{name} {BuyPrice} {growTime} {sellPrice}");
     }else if(!parseInt(getCommand()[2])>0){
       send(parseInt(getCommand()[2]));
       send(getCommand()[2]);
-      send("cena musi być liczbą!!")
+      send("Price must be a number!!")
     }else if(!parseInt(getCommand()[3])>0){
-      send("czas musi być liczbą (pisz w minutach)")
+      send("time must be a number(write in minutes)")
     }else if(!parseInt(getCommand()[4])>0){
-      send("cenaprzedarzy musi być liczbą (pisz w sekundach)")
+      send("sellPrice myst be a number")
     }else{
       plantsData[getCommand()[1]]={
         "buyPrice":parseInt(getCommand()[2]),
@@ -132,29 +111,27 @@ client.on('message', msg => {
   //normal user
 
   if (getCommand()[0]=="#wallet") {
-    wallet();
+    send("You have:"+playerData[msg.author.id].money);
   }
 
   if (getCommand()[0]=="#createFarm") {
     if (getCommand().length!=2) {
       send("{size}");
     }else if(!parseInt(getCommand()[1])>0){
-      send("wielkość musi być liczbą!!");
+      send("size must be a number!!");
     }else{
-      if (debuging) {send("{debug} rozpoczęto tworzenie")}
       let farmSize=parseInt(getCommand()[1])*parseInt(getCommand()[1]);
       let farmPrice=farmSize*10;
-      if (debuging) {send("{debug} "+farmSize+" "+farmPrice)}
       if (playerData[msg.author.id].money>=farmPrice) {
         playerData[msg.author.id].farm.size=farmSize;
         playerData[msg.author.id].money-=farmPrice;
-        send("miałeś:"+playerData[msg.author.id].money);
-        send("farma kosztuje:"+farmPrice);
-        send("masz:"+playerData[msg.author.id].money)
-        send("twoja farma ma teraz:"+farmSize+"pól");
+        send("u had:"+playerData[msg.author.id].money);
+        send("farm price:"+farmPrice);
+        send("now have:"+playerData[msg.author.id].money)
+        send("your farm now contain:"+farmSize+"dirt tiles");
       }else{
-        wallet();
-        send("fama kosztuje:"+farmPrice+"\n potzebujesz:"+farmPrice-playerData[msg.author.id].money);
+        send("You have:"+playerData[msg.author.id].money);
+        send("farm cost:"+farmPrice+"\n u need:"+farmPrice-playerData[msg.author.id].money);
       }
     }
   }
